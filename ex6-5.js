68a1ed9ac103cfb4b43616421de5b4a5a774c816
marikky <131091904+marikky@users.noreply.github.com>
2023-10-17 09:04:06 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,4 +1,13 @@
 var who65 = 'world';
+var who66 = 'world';
+
 function sayhello65(){alert('Hello,'+ who65 + '!');}
 function taro65(){who65 = '太郎';}
 function hanako65(){who65 = '花子';}
+
+function taro66(){who66 = '太郎が来た';}
+function hanako66(){who66 = '花子が来た';}
+
+
+
+
