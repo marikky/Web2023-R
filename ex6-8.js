11377da1aefb60488d2a31ec719a51032b6614a2
marikky <131091904+marikky@users.noreply.github.com>
2023-10-23 21:49:24 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -4,6 +4,6 @@ function hanako68(){document.getElementById('who68').innerHTML = '花子';}
 
 
 
-function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/bulbon.gif" alt="オン">';}
+function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/kid_seikaku_kachiki_boy" alt="オン">';}
 <img src="./image/kid_seikaku_kachiki_boy.png" alt="いらすとや　中肉中背の男性" width="400" height="600">
 <img src="./image/macho_man.png" alt="いらすとや　マッチョの男性" width="400" height="600">
