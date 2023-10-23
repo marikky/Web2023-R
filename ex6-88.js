ad51d142fa3e0cea0a1e6d0d3bbe0c0016075e56
marikky <131091904+marikky@users.noreply.github.com>
2023-10-23 21:59:17 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,3 +1,2 @@
 function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/macho_man.png" alt="オン">';}
-<img src="./image/kid_seikaku_kachiki_boy.png" alt="いらすとや　中肉中背の男性" width="400" height="600">
-<img src="./image/macho_man.png" alt="いらすとや　マッチョの男性" width="400" height="600">
+
