e546266686ab817d890a8ef53f6f06932fbd4c6c
marikky <131091904+marikky@users.noreply.github.com>
2023-10-23 20:38:48 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,6 +1,6 @@
-function sayhello68(){alert('Hello,'+ document.getElementByID('who68').innerHTML +'!');}
-function taro68(){document.getElementByID('who68').innerHTML = '太郎';}
-function taro68(){document.getElementByID('who68').innerHTML = '花子';}
+function sayhello68(){alert('Hello,'+ document.getElementById('who68').innerHTML +'!');}
+function taro68(){document.getElementById('who68').innerHTML = '太郎';}
+function taro68(){document.getElementById('who68').innerHTML = '花子';}
 
 
 
