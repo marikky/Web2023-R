9f859ea6e570d3045592c6e9d2cfa81409aa7f4a
marikky <131091904+marikky@users.noreply.github.com>
2023-10-31 08:44:05 +0900

    Update Jsmtm.js

--- Jsmtm.js
+++ Jsmtm.js
@@ -1,3 +1,3 @@
 var img;
-function gazo23(){document.getElementById('').src='./image/necchusyou_face_boy1.png';}
-function gazo22(){document.getElementById('').src='./image/face_smile_man1.png';}
+function gazo23(){document.getElementById('img23').src='./image/necchusyou_face_boy1.png';}
+function gazo22(){document.getElementById('img23').src='./image/face_smile_man1.png';}
