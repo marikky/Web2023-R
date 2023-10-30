a2b565aec93fb485c30d8ee633552dd66d6a9a88
marikky <131091904+marikky@users.noreply.github.com>
2023-10-31 08:41:21 +0900

    Update Jsmtm.js

--- Jsmtm.js
+++ Jsmtm.js
@@ -1,1 +1,2 @@
 function gazo23(){document.getElementById('img23').innerHTML='<img src="./image/necchusyou_face_boy1.png" alt="オン">';}
+function gazo22(){document.getElementById('img23').innerHTML='<img src="./image/face_smile_man1.png" alt="オン">';}
