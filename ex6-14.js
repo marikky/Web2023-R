c8bcb35d5bb116a47b1086b68ad4ae5b56d93a17
marikky <131091904+marikky@users.noreply.github.com>
2023-11-07 10:25:05 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,3 @@
 function gazo3(){
-<img src='./image/back_alley_2.jpg' id="img3" alt="gazohen" class="imagechange">
+<img src='./image/_residential_street_2.jpg' id="img3" alt="gazohen" class="imagechange">
 setTimeout("gazo3()",3000);}
