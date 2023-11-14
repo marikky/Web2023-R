bccbd6c414a96e2e17282bd319eedacb5fd3c8fb
marikky <131091904+marikky@users.noreply.github.com>
2023-11-14 09:51:49 +0900

    Update ex6-15.js

--- ex6-15.js
+++ ex6-15.js
@@ -2,6 +2,6 @@
 //<img src='./image/_residential_street_2.jpg' id="img33" alt="gazohen3" class="imagechange3">
 //setTimeout("gazo33()",3000);}
 var img;
-function gazo33(){document.getElementById('img33').src='./image/_residential_street_2.jpg'; setTimeout("gazo34()",3000);}
+function gazo33(){document.getElementById('img33').src='./image/back_alley_3.jpg'; setTimeout("gazo34()",3000);}
 function gazo34(){document.getElementById('img33').src='./image/back_alley_2.jpg';}
 
