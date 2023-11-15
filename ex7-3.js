cd0317d428d58b7163bdfc6f8d24603e0a7cf52b
marikky <131091904+marikky@users.noreply.github.com>
2023-11-15 15:08:54 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,3 +1,3 @@
 function add(){var x = document.getElementById('input1').value;
                var y = document.getElementById('input2').value;
-               alert(x/y**y);}
+               alert(x/y*y);}
