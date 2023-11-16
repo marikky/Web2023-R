40dd126571cdfe3040561ac928f654c38ecf9a83
marikky <131091904+marikky@users.noreply.github.com>
2023-11-16 15:33:29 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,4 +1,4 @@
-function add(){
+function bmi5(){
                var Number(x) = document.getElementById('bmi1').value;
                var Number(y) = document.getElementById('bmi2').value;
                alert(Number(x)/Number(y)*Number(y));}
