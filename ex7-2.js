917c9958a061b9722fb7a97c1bcd59bc9099b0c4
marikky <131091904+marikky@users.noreply.github.com>
2023-11-16 15:26:24 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,4 +1,13 @@
 function add(){ 
                var x = document.getElementById('input1').value;
                var y = document.getElementById('input2').value;
-               alert(x+y);}
+               alert(Number(x) + Number(y));}
+
+
+//var a = "3";
+//var b = "2";
+
+//ar ans = Number(a) + Number(b);
+// Javascriptでは文字数字をNumber()関数で囲う
+//console.log(ans);
+//
