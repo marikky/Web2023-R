80d619bb2465c60bab4091c5ee88fb819e423c54
marikky <131091904+marikky@users.noreply.github.com>
2023-11-16 16:03:56 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,18 +1,18 @@
-function bmi5(){ var x,y
-               x = document.getElementById('bmi1').value;
-               y = document.getElementById('bmi2').value;
-               alert(Number(x) / Number(y) * Number(y));}
+//function bmi5(){ var x,y
+               //x = document.getElementById('bmi1').value;
+               //y = document.getElementById('bmi2').value;
+               //alert(Number(x) / Number(y) * Number(y));}
 
 
-//function keisan(){ //keisanという関数を実行
-  //var a, b,d;
-  //a = document.getElementById("shincho").value; // "sincho"で記入した値をaに代入
- // b	= document.getElementById("taijuu").value; // "taijyuu"で記入した体重をbに代入
-  //c = b/( (a/100)*(a/100) ); //BMIの計算結果をcに代入
+function keisan(){ //keisanという関数を実行
+  var a, b,d;
+  a = document.getElementById("shincho").value; // "sincho"で記入した値をaに代入
+  b	= document.getElementById("taijuu").value; // "taijyuu"で記入した体重をbに代入
+  c = b/( (a/100)*(a/100) ); //BMIの計算結果をcに代入
 
- //if(c >= 25){ //cが25以上の場合〇〇に”肥満型”と表示される
-   //document.getElementById("kuuran").textContent = "肥満型"; }
-  //else if(c < 25 && c >= 18){ //cが18〜25の場合〇〇に”標準型”と表示される
-  // document.getElementById("kuuran").textContent = "標準型"; }
- // else{ //それ以外は〇〇に”痩せ型”と表示される
-   //document.getElementById("kuuran").textContent = "瘦せ型";}}
+ if(c >= 25){ //cが25以上の場合〇〇に”肥満型”と表示される
+   document.getElementById("kuuran").textContent = "肥満型"; }
+  else if(c < 25 && c >= 18){ //cが18〜25の場合〇〇に”標準型”と表示される
+   document.getElementById("kuuran").textContent = "標準型"; }
+  else{ //それ以外は〇〇に”痩せ型”と表示される
+   document.getElementById("kuuran").textContent = "瘦せ型";}}
