5b7cf33b1f9bfd571b9c59b04f5170f50fd0bdea
marikky <131091904+marikky@users.noreply.github.com>
2023-11-16 15:22:29 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,4 +1,4 @@
-function add(){
-               var Number(x) = document.getElementById('input1').value;
-               var Number(y) = document.getElementById('input2').value;
+function add(){ 
+               var x = document.getElementById('input1').value;
+               var y = document.getElementById('input2').value;
                alert(x+y);}
