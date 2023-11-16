e88c4e69d108a4ebad8ec7b5463ef5a8cf846852
marikky <131091904+marikky@users.noreply.github.com>
2023-11-16 15:53:09 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,7 +1,7 @@
 function bmi5(){
                var x = document.getElementById('bmi1').value;
                var y = document.getElementById('bmi2').value;
-               alert(y/( (x/100)*(x/100) );}
+               alert(Number(x) / Number(y) * Number(y));}
 
 
 //function keisan(){ //keisanという関数を実行
