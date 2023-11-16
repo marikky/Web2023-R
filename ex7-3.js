2fbb2824d08c60992996f295c6744c07f3da6dd7
marikky <131091904+marikky@users.noreply.github.com>
2023-11-16 15:46:48 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,4 +1,11 @@
 function bmi5(){
                var x = document.getElementById('bmi1').value;
                var y = document.getElementById('bmi2').value;
-               alert(Number(x)/Number(y)*Number(y));}
+               alert(y/( (x/100)*(x/100) );}
+
+
+//function keisan(){ //keisanという関数を実行
+  //var a, b,d;
+  //a = document.getElementById("shincho").value; // "sincho"で記入した値をaに代入
+  //b	= document.getElementById("taijuu").value; // "taijyuu"で記入した体重をbに代入
+  //c = b/( (a/100)*(a/100) ); //BMIの計算結果をcに代入
