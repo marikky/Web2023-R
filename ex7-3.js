20b8eada801cbd192e44295aef6b561dadaaf977
marikky <131091904+marikky@users.noreply.github.com>
2023-11-28 08:54:54 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -11,10 +11,3 @@ function keisan(){ //keisanという関数を実行
   c = b/( (a/100)*(a/100) ); //BMIの計算結果をcに代入
 　alert(c);
   
-// if(c >= 25){ //cが25以上の場合〇〇に”肥満型”と表示される
- //  document.getElementById("kuuran").textContent = "肥満型"; }
-//  else if(c < 25 && c >= 18){ //cが18〜25の場合〇〇に”標準型”と表示される
-//   document.getElementById("kuuran").textContent = "標準型"; }
-//  else{ //それ以外は〇〇に”痩せ型”と表示される
-//   document.getElementById("kuuran").textContent = "瘦せ型";}
-}
