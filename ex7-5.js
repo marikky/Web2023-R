5a7c44e3795a9bccb078c927dc961136789fc265
marikky <131091904+marikky@users.noreply.github.com>
2023-11-28 08:51:39 +0900

    Update ex7-5.js

--- ex7-5.js
+++ ex7-5.js
@@ -1,2 +1,2 @@
 function nazo1(){
-                if(document.getElementById('answer').value == '夢'){alert('あたり！');}else{alert('はずれ...');}}
+                if(document.getElementById('answer1').value == '夢'){alert('あたり！');}else{alert('はずれ...');}}
