0ba60075e2451d8b73f1b99923960e421420c50d
marikky <131091904+marikky@users.noreply.github.com>
2023-11-28 18:36:40 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,7 +1,18 @@
 
-function uru{
+//function uru{
 
-let year = "2400"
+//let year = "2400"
 
-	if ( (!(year % 100 == 0) && (year % 4 == 0)) || ((year % 100 == 0) && (year % 400 == 0)) )
-  {year = year + "閏年です。";} else {year = year + "平年です。";}};
+	//if ( (!(year % 100 == 0) && (year % 4 == 0)) || ((year % 100 == 0) && (year % 400 == 0)) )
+ //{year = year + "閏年です。";} else {year = year + "平年です。";}};
+
+
+function uru() {
+    var year = Number(document.getElementById('seireki').value); 
+	var result;
+
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {kotae = '閏年ですよ';} 
+                                                      else {kotae = '平年でした';}
+
+    document.getElementById('kotae2').innerText = year + result + 'です。';
+    alert( year + kotae + 'です。');}
