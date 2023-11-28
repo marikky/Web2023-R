cc982ed62f4061e59e987bfd51fa078b8aa63c0e
marikky <131091904+marikky@users.noreply.github.com>
2023-11-28 10:34:33 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,1 +1,10 @@
 
+function uru{
+  
+//----] 西暦年を入力から取得
+const Seireki = Number( window.prompt( "西暦年を入力してください" ) );
+//----] うるう年を判別
+if( Seireki%400===0 ) { alert( "閏年です" ) }
+else if( Seireki%100===0 ) { alert( "平年です" ) }
+else if( Seireki%4===0 ) { alert( "閏年です" ) }
+else { alert( "平年です" ) }}
