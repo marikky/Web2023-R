781d841609a82dff0f66e1c72abc3b61fea0b8b9
marikky <131091904+marikky@users.noreply.github.com>
2023-12-05 09:24:31 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -1,10 +1,24 @@
-function keisan1(){ 
+// function keisan1(){ 
+//  var a, b,d;
+//  a = document.getElementById("shincho1").value; 
+//  b	= document.getElementById("taijuu1").value; 
+//  c = b/( (a/100)*(a/100) ); 
+
+// if(c >= 25){document.getElementById("kuuran1").textContent = "肥満"; }
+//  else if(c < 25 && c >= 18.5){document.getElementById("kuuran1").textContent = "普通"; }
+//  else{document.getElementById("kuuran1").textContent = "低体重";}
+//  {alert("kuuran1");}}
+
+function hantei2(){ 
   var a, b,d;
-  a = document.getElementById("shincho1").value; 
-  b	= document.getElementById("taijuu1").value; 
+  a = document.getElementById("height").value; 
+  b	= document.getElementById("bodyweight").value; 
   c = b/( (a/100)*(a/100) ); 
 
- if(c >= 25){document.getElementById("kuuran1").textContent = "肥満"; }
-  else if(c < 25 && c >= 18.5){document.getElementById("kuuran1").textContent = "普通"; }
-  else{document.getElementById("kuuran1").textContent = "低体重";}
-  {alert("kuuran1");}}
+ if(c >= 25){ 
+   document.getElementById("haku").textContent = "肥満"; }
+  else if(c < 25 && c >= 18.5){ document.getElementById("haku").textContent = "普通"; }
+  else{ document.getElementById("haku").textContent = "低体重";}
+  { alert("haku"); }}
+
+
