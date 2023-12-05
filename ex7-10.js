6cf0379d0a21ed6af5b65f9b2ee30f8aff6a91fd
marikky <131091904+marikky@users.noreply.github.com>
2023-12-05 09:33:00 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -21,3 +21,13 @@ function hantei2(){
   { alert('あなたのBMI指数は' + "haku" + 'です'); }}
 
 
+//function BMI2() {
+//    var height2 = Number(document.getElementById('height2').value) / 100;
+//    var weight2 = Number(document.getElementById('weight2').value);
+//    var bmi2 = weight2 / (height2 * height2); 
+
+    //var classification;
+    //if (bmi2 < 18.5) {classification = '低体重';} 
+    //else if (bmi2 < 25) {classification = '普通';}
+    //else {classification = '肥満';}
+    //alert('あなたのBMIは ' + bmi2 + ' です。この数値は「' + classification + '」の範囲に分類されます。');}
