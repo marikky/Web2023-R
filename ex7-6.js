bc44b7ebecf1ca2fdb4663695da08406c42683d9
marikky <131091904+marikky@users.noreply.github.com>
2023-12-19 09:17:28 +0900

    Update ex7-6.js

--- ex7-6.js
+++ ex7-6.js
@@ -1,2 +1,2 @@
 function nazo2(){
-                if(document.getElementById('answer2').value != '夢'){alert('あたり！');}else{alert('はずれ...');}}
+                if(document.getElementById('answer2').value != '夢'){alert('はずれ...');}else{alert('あたり！');}}
